2e0a36a755d8d0364465b5158c625c91114f80b4
respinha <ruiespinharibeiro.ua.pt>
2016-09-19 19:00:45 +0100

    Building constructor

--- FCM.h
+++ FCM.h
@@ -3,14 +3,15 @@
 class FCM {
 
 	public:
-		FCM(int nLetters, string srcText);
-		void updateContext(int nLetters);
+		FCM(int order, string srcText);
+		void updateContext(int order);
 		void updateSrcText(string srcText);
 		void printResult();
 		void printTable();
 
 	private:
 		int lut[][];	
+		static int N_ALPHABET;
 		buildLUT();
 
 }
