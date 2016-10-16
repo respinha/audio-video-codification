0fae67ba36bc628b5cfa5f81c64fbb43fbe62a9b
= <ruiespinharibeiro.ua.pt>
2016-10-16 23:10:44 +0100

    Added some logic for later discussion

--- audio-encoder/BitStream.cpp
+++ audio-encoder/BitStream.cpp
@@ -1,15 +1,23 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <stringstream>
 
 BitStream::BitStream(string filename) {
 // read file
 }
 
-byte BitStream::readBit(
-	return 'c';
+byte BitStream::readBit (byte word) (
+	return (word >> 7) & 0x1;
 )
 
-byte[] BitStream::readNBits(
-	return new byte[1];
+byte[] BitStream::readNBits (byte word, int n) (
+	byte[] bits = new byte[n];
+	for(int i = 0; i < n; i++) {
+		// 
+		std::stringstream ss;
+		ss<< hex << (i+1); 
+		byte[i] = (word >> (7-i)) & (ss >> i);
+	}
+	return bits;
 }
